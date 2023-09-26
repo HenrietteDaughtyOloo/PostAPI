8123c4450bae0e09b19a2aeaedcc5cb7d8983581
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-09-26 07:10:27 +0300

    Created Post request

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import useGetPosts from "./hooks/useGetPosts"
+export default function Home() {
+
+  const posts=useGetPosts();
+  console.log({newPosts:posts});
+  
+  return (
+    <main>
+              <h1>POSTS</h1>
+
+      <div className="grid grid-cols-4 gap-4 p-4">
+        {posts.posts.map(item=>(
+          <div key={item.id} className="border border-[#7e7e7e] p-4 rounded">
+            <h2 className="font-bold">{item.title}</h2>
+            <p>{item.body}</p>
+
+          </div>
+        ))}
+      </div>
+    </main>
+  )
+}
